007ab551739680af82122530579b379cead754b7
Simon Gifford <simon.gifford@mashauri.org>
2025-08-05 18:32:56 +0000

    change to lib

--- atooverride/lib.php
+++ atooverride/lib.php
@@ -3,5 +3,5 @@ defined('MOODLE_INTERNAL') || die();
 
 function local_attooverride_before_footer() {
     global $PAGE;
-    $PAGE->requires->js_call_amd('local_attooverride/override', 'init');
+    $PAGE->requires->js(new moodle_url('/local/attooverride/amd/src/override.js'));
 }
