15f13c53a1f443f3ccb342e0dd641d81fb1aab53
Simon Gifford <simon.gifford@mashauri.org>
2025-08-06 10:57:47 +0200

    Update override.js to hack version

--- atooverride/amd/src/override.js
+++ atooverride/amd/src/override.js
@@ -1,10 +1,23 @@
 (function() {
-    require(['editor_atto/plugins/fontcolor/colorpicker'], function(picker) {
-        picker.prototype.colors = [
-            'cd1d31', // Your brand color
-            '000000', '888888', 'FFFFFF',
-            'FF0000', '00FF00', '0000FF',
-            'FFFF00', '00FFFF', 'FF00FF'
-        ];
-    });
+    const waitForPicker = () => {
+        if (typeof M !== 'undefined' &&
+            M.editor_atto &&
+            M.editor_atto.plugins &&
+            M.editor_atto.plugins.fontcolor &&
+            M.editor_atto.plugins.fontcolor.button &&
+            M.editor_atto.plugins.fontcolor.button.prototype.colors) {
+            
+            M.editor_atto.plugins.fontcolor.button.prototype.colors = [
+                'cd1d31', // Your brand color
+                '000000', '888888', 'FFFFFF',
+                'FF0000', '00FF00', '0000FF',
+                'FFFF00', '00FFFF', 'FF00FF'
+            ];
+        } else {
+            setTimeout(waitForPicker, 100);
+        }
+    };
+
+    waitForPicker();
 })();
+
